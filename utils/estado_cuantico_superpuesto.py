42eaaf702cf71376eced7ddb14e5caf607b1fa56
jako <jakocrazykings@gmail.com>
2025-03-31 11:14:31 -0600

    Actualizar utils
    estado base en superposición representado clásicamente

---
+++ utils/estado_cuantico_superpuesto.py
@@ -0,0 +1,8 @@
+estado_superpuesto = {
+    "00": (0.707 + 0j),  # Amplitud compleja del estado |00⟩ (ejemplo)
+    "01": (0.0 + 0.0j),
+    "10": (0.0 + 0.0j),
+    "11": (0.707 + 0j)   # Amplitud compleja del estado |11⟩ (ejemplo)
+}
+
+variable_estado = estado_superpuesto
